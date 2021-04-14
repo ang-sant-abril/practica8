2d51a58449c5a4fe1e1899596be3533794da4490
ang-sant-abril <ang-sant-abril@gmail.com>
2021-04-14 07:36:06 +0000

    ends

---
+++ src/app/top-score/top-score.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-score',
+  templateUrl: './top-score.component.html',
+  styleUrls: ['./top-score.component.css']
+})
+export class TopScoreComponent implements OnInit {
+
+  @Input() jugadoresLocales: any;
+  @Input() jugadoresVisitantes: any;
+  jugadores: any = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.jugadores = [...this.jugadoresLocales, ...this.jugadoresVisitantes];
+  }
+
+  ngAfterContentChecked() {
+    this.sortJugadores() 
+  }
+
+  sortJugadores() {
+    this.jugadores.sort((a: any, b: any) => {
+      return (b.puntos - a.puntos);
+    })
+  }
+
+}
